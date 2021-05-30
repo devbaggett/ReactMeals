85a95ae16154bce8f54ff0acb33a2607cba7106f
devbaggett <devabaggett@gmail.com>
2021-05-30 10:58:12 -0700

    Added error handling.

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -5,10 +5,18 @@ import MealItem from "./MealItem/MealItem";
 
 function AvailableMeals() {
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [httpError, setHttpError] = useState();
 
     useEffect(() => {
         const fetchMeals = async () => {
-            const response = await fetch("https://react-meals-ee7bf-default-rtdb.firebaseio.com/meals.json");
+            setIsLoading(true);
+            const response = await fetch("https://react-meals-ee7bf-default-rtdb.firebaseio.com/meals");
+
+            if (!response.ok) {
+                throw new Error("Something went wrong!");
+            }
+
             const responseData = await response.json();
 
             const loadedMeals = [];
@@ -21,9 +29,30 @@ function AvailableMeals() {
                 });
             }
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
-        fetchMeals();
+        fetchMeals()
+            .catch(error => {
+                setIsLoading(false);
+                setHttpError(error.message);
+            });
     }, []);
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
+            </section>
+        );
+    }
 
     const mealsList = meals.map(meal => (
         <MealItem key={meal.id}
